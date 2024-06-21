7c7c241ea499fdeea5c2734e7f3322154f1b8e03
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2024-06-21 18:07:38 +0900

    mix new zcam_elixir

---
+++ zcam_elixir/lib/zcam_elixir.ex
@@ -0,0 +1,18 @@
+defmodule ZcamElixir do
+  @moduledoc """
+  Documentation for `ZcamElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ZcamElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ zcam_elixir/test/zcam_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule ZcamElixirTest do
+  use ExUnit.Case
+  doctest ZcamElixir
+
+  test "greets the world" do
+    assert ZcamElixir.hello() == :world
+  end
+end
